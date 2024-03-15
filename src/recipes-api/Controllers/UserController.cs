c4006d7d04478c62b3241f1edfb10a4b3db0c5f4
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-03-15 11:52:40 -0300

    req 6,7,8,9,10,11

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -12,39 +12,74 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
+{
     public readonly IUserService _service;
-    
+
     public UserController(IUserService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 6 - Sua aplicação deve ter o endpoint GET /user/:email
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
+    {
+        try
+        {
+            bool exists = _service.UserExists(email);
+            if (exists)
+            {
+                User user = _service.GetUser(email);
+                return Ok(user);
+            }
+            throw new Exception("");
+        }
+        catch (Exception err)
+        {
+            return NotFound(err.Message);
+        }
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
     [HttpPost]
-    public IActionResult Create([FromBody]User user)
+    public IActionResult Create([FromBody] User user)
     {
-        throw new NotImplementedException();
+        _service.AddUser(user);
+        return Created("", user);
     }
 
     // "8 - Sua aplicação deve ter o endpoint PUT /user
     [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
+    public IActionResult Update(string email, [FromBody] User user)
     {
-        throw new NotImplementedException();
+        try
+        {
+            bool exist = _service.UserExists(email);
+            if (exist)
+            {
+                _service.UpdateUser(user);
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        catch (Exception err)
+        {
+            return BadRequest(err.Message);
+        }
     }
 
     // 9 - Sua aplicação deve ter o endpoint DEL /user
     [HttpDelete("{email}")]
     public IActionResult Delete(string email)
     {
-        throw new NotImplementedException();
-    } 
+        bool e = _service.UserExists(email);
+        if (e)
+        {
+            return NoContent();
+        }
+        return NotFound();
+    }
 }
